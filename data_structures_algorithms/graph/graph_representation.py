fdf1f4c42df45eeb89aa40725343e8c58d051ca5
YUVRAJ SINGH <yuvraj@yuvrajtech.com>
2025-07-14 01:34:26 +0530

    basic graph representation

--- data_structures_algorithms/graph/graph_representation.py
+++ data_structures_algorithms/graph/graph_representation.py
@@ -1,9 +1,39 @@
 # Graph Representation
 
 # Edge List
+edge_list = [
+    [0,1],
+    [1,2],
+    [0,3],
+    [3,4],
+    [3,6],
+    [3,7],
+    [4,2],
+    [4,5],
+    [5,2]
+]
 
 # Adjacency Matrix
+matrix = []
+n = 8
+for i in range(0,n):
+    matrix.append([0]*n)
+    
+for u,v in edge_list:
+    matrix[u][v] = 1
+    
+for i in range(0,n):
+    print(matrix[i])
 
 # Adjacency List
+from collections import defaultdict
+
+adjacency_list = defaultdict(list)
+
+for u,v in edge_list:
+    adjacency_list[u].append(v)
+    
+for i in adjacency_list:
+    print(f"{i} : {adjacency_list[i]}")
 
 # Class Based
